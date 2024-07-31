bf752f0aa38a71f8e020c0a7d2c97e9a4d7ca8bc
IanNjeru17 <iannjeru17@gmail.com>
2024-07-31 14:45:36 +0300

    add

--- src/components/App.js
+++ src/components/App.js
@@ -15,14 +15,22 @@ function Home() {
     </div>
   );
 }
-
-{/* write an <About> component here */}
+function About()
+{
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
